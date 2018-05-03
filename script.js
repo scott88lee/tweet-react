db9b2d195402bac4d2a4e2bf5007137dde591a38
scott88lee <scott88lee@gmail.com>
2018-05-03 19:16:39 +0800

    basic

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-var monkey = "hello";
+class Tweet_Item extends React.Component {
+    render() {
+        return (
+          <div>
+            <h3>{this.props.screen_name}</h3>
+            <h5>{this.props.display_url}</h5>
+            <p>ID => {this.props.index}</p>
+            <p>Text => {this.props.text}</p>
+            <hr></hr>
+          </div>
+        );
+    }
+}
+
+class Tweet_list extends React.Component {
+	render() {
+		let tweetComponents = this.props.tweets.map( (item, index) => {
+			return <Tweet_Item key={item.id} index={item.id} text={item.text} screen_name={item.user.screen_name} display_url={item.user.entities.url.urls[0].display_url.toLowerCase()}>
+              	   </Tweet_Item>
+		});
+		return (
+			<div>
+           		{tweetComponents}
+        	</div>
+        );
+	}
+}
+
 ReactDOM.render(
-    <div>
-      {monkey}
-    </div>,
+    <Tweet_list tweets={tweets}/>,
     document.getElementById('root')
 );
